c826e7616eb349918e3f7cbccd966ecca57294a7
Facundo Ferrin <facundo.ferrin@gmail.com>
2024-02-17 08:22:15 -0800

    Add button on Medium pages

--- extension/content.js
+++ extension/content.js
@@ -1,8 +1,34 @@
-const freediumUrl = "https://freedium.cfd/";
-const regex = /(https:\/\/(?:\w+\.)?medium\.com\/@[^\/]+\/*)/g;
+function freeUrl(url) {
+  const urlObj = new URL(url)
 
-window.onclick = function (event) {
-  if (event.target instanceof HTMLAnchorElement && !event.target.href.startsWith(freediumUrl)) {
-    event.target.href = event.target.href.replace(regex, freediumUrl);
+  urlObj.host = "freedium.cfd";
+
+  return urlObj.href;
+}
+
+document.addEventListener("readystatechange", (event) => {
+  if (window.location.host.endsWith("medium.com")) {
+    const button = document.createElement("button");
+    button.id = "free-medium__button";
+    button.textContent = "Read for free";
+    button.style.position = "fixed";
+    button.style.bottom = "20px";
+    button.style.right = "20px";
+    button.style.padding = "20px 28px";
+    button.style.backgroundColor = "#007bff";
+    button.style.color = "#fff";
+    button.style.border = "none";
+    button.style.borderRadius = "48px";
+    button.style.fontSize = "18px";
+    button.style.cursor = "pointer";
+    button.style.zIndex = "9999";
+    button.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.2)";
+
+    button.addEventListener("click", function () {
+      window.location.href = freeUrl(document.URL);
+    });
+
+    document.body.appendChild(button);
   }
-};
+});
+
